17e4844a01ebc0aa34a3cf06067607555273042c
Rasmus Diederichsen <rasmus@peltarion.com>
2018-07-03 15:14:07 +0200

    Apply autopep8

--- cca_core.py
+++ cca_core.py
@@ -1,19 +1,19 @@
 # Copyright 2016 Google Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 
-"""
+'''
 The core code for applying Canonical Correlation Analysis to deep networks.
 
 This module contains the core functions to apply canonical correlation analysis
@@ -28,7 +28,7 @@ the dft_ccas (Discrete Fourier Transform) module can be used.
 
 See https://arxiv.org/abs/1706.05806 for full details.
 
-"""
+'''
 
 
 import numpy as np
@@ -38,7 +38,7 @@ epsilon = 1e-6
 
 
 def positivedef_matrix_sqrt(array):
-    """Stable method for computing matrix square roots, supports complex matrices.
+    '''Stable method for computing matrix square roots, supports complex matrices.
 
     Args:
               array: A numpy 2d array, can be complex valued that is a positive
@@ -46,7 +46,7 @@ def positivedef_matrix_sqrt(array):
 
     Returns:
               sqrtarray: The matrix square root of array
-    """
+    '''
     w, v = np.linalg.eigh(array)
     #  A - np.dot(v, np.dot(np.diag(w), v.T))
     wsqrt = np.sqrt(w)
@@ -55,7 +55,7 @@ def positivedef_matrix_sqrt(array):
 
 
 def remove_small(sigma_xx, sigma_xy, sigma_yx, sigma_yy, threshold=1e-6):
-    """Takes covariance between X, Y, and removes values of small magnitude.
+    '''Takes covariance between X, Y, and removes values of small magnitude.
 
     Args:
               sigma_xx: 2d numpy array, variance matrix for x
@@ -73,7 +73,7 @@ def remove_small(sigma_xx, sigma_xy, sigma_yx, sigma_yy, threshold=1e-6):
               sigma_yy_crop: 2d array with low y norm directions removed
               x_idxs: indexes of sigma_xx that were removed
               y_idxs: indexes of sigma_yy that were removed
-    """
+    '''
 
     x_diag = np.abs(np.diagonal(sigma_xx))
     y_diag = np.abs(np.diagonal(sigma_yy))
@@ -90,7 +90,7 @@ def remove_small(sigma_xx, sigma_xy, sigma_yx, sigma_yy, threshold=1e-6):
 
 
 def compute_ccas(sigma_xx, sigma_xy, sigma_yx, sigma_yy, verbose=True):
-    """Main cca computation function, takes in variances and crossvariances.
+    '''Main cca computation function, takes in variances and crossvariances.
 
     This function takes in the covariances and cross covariances of X, Y,
     preprocesses them (removing small magnitudes) and outputs the raw results of
@@ -122,7 +122,7 @@ def compute_ccas(sigma_xx, sigma_xy, sigma_yx, sigma_yy, verbose=True):
               x_idxs:       The indexes of the input sigma_xx that were pruned
                             by remove_small
               y_idxs:       Same as above but for sigma_yy
-    """
+    '''
 
     (sigma_xx, sigma_xy, sigma_yx, sigma_yy, x_idxs, y_idxs) = remove_small(
         sigma_xx, sigma_xy, sigma_yx, sigma_yy)
@@ -135,19 +135,19 @@ def compute_ccas(sigma_xx, sigma_xy, sigma_yx, sigma_yy, verbose=True):
                 np.zeros_like(sigma_yy), x_idxs, y_idxs)
 
     if verbose:
-        print("adding eps to diagonal and taking inverse")
+        print('adding eps to diagonal and taking inverse')
     sigma_xx += epsilon * np.eye(numx)
     sigma_yy += epsilon * np.eye(numy)
     inv_xx = np.linalg.pinv(sigma_xx)
     inv_yy = np.linalg.pinv(sigma_yy)
 
     if verbose:
-        print("taking square root")
+        print('taking square root')
     invsqrt_xx = positivedef_matrix_sqrt(inv_xx)
     invsqrt_yy = positivedef_matrix_sqrt(inv_yy)
 
     if verbose:
-        print("dot products...")
+        print('dot products...')
     arr_x = np.dot(sigma_yx, invsqrt_xx)
     arr_x = np.dot(inv_yy, arr_x)
     arr_x = np.dot(invsqrt_xx, np.dot(sigma_xy, arr_x))
@@ -156,7 +156,7 @@ def compute_ccas(sigma_xx, sigma_xy, sigma_yx, sigma_yy, verbose=True):
     arr_y = np.dot(invsqrt_yy, np.dot(sigma_yx, arr_y))
 
     if verbose:
-        print("trying to take final svd")
+        print('trying to take final svd')
     arr_x_stable = arr_x + epsilon * np.eye(arr_x.shape[0])
     arr_y_stable = arr_y + epsilon * np.eye(arr_y.shape[0])
     ux, sx, vx = np.linalg.svd(arr_x_stable)
@@ -165,13 +165,13 @@ def compute_ccas(sigma_xx, sigma_xy, sigma_yx, sigma_yy, verbose=True):
     sx = np.sqrt(np.abs(sx))
     sy = np.sqrt(np.abs(sy))
     if verbose:
-        print("computed everything!")
+        print('computed everything!')
 
     return [ux, sx, vx], [uy, sy, vy], invsqrt_xx, invsqrt_yy, x_idxs, y_idxs
 
 
 def sum_threshold(array, threshold):
-    """Computes threshold index of decreasing nonnegative array by summing.
+    '''Computes threshold index of decreasing nonnegative array by summing.
 
     This function takes in a decreasing array nonnegative floats, and a
     threshold between 0 and 1. It returns the index i at which the sum of the
@@ -183,8 +183,8 @@ def sum_threshold(array, threshold):
 
     Returns:
               i: index at which np.sum(array[:i]) >= threshold
-    """
-    assert (threshold >= 0) and (threshold <= 1), "print incorrect threshold"
+    '''
+    assert (threshold >= 0) and (threshold <= 1), 'print incorrect threshold'
 
     for i in range(len(array)):
         if np.sum(array[:i]) / np.sum(array) >= threshold:
@@ -192,7 +192,7 @@ def sum_threshold(array, threshold):
 
 
 def create_zero_dict(compute_dirns, dimension):
-    """Outputs a zero dict when neuron activation norms too small.
+    '''Outputs a zero dict when neuron activation norms too small.
 
     This function creates a return_dict with appropriately shaped zero entries
     when all neuron activations are very small.
@@ -203,25 +203,25 @@ def create_zero_dict(compute_dirns, dimension):
 
     Returns:
               return_dict: a dict of appropriately shaped zero entries
-    """
+    '''
     return_dict = {}
-    return_dict["mean"] = (np.asarray(0), np.asarray(0))
-    return_dict["sum"] = (np.asarray(0), np.asarray(0))
-    return_dict["cca_coef1"] = np.asarray(0)
-    return_dict["cca_coef2"] = np.asarray(0)
-    return_dict["idx1"] = 0
-    return_dict["idx2"] = 0
+    return_dict['mean'] = (np.asarray(0), np.asarray(0))
+    return_dict['sum'] = (np.asarray(0), np.asarray(0))
+    return_dict['cca_coef1'] = np.asarray(0)
+    return_dict['cca_coef2'] = np.asarray(0)
+    return_dict['idx1'] = 0
+    return_dict['idx2'] = 0
 
     if compute_dirns:
-        return_dict["cca_dirns1"] = np.zeros((1, dimension))
-        return_dict["cca_dirns2"] = np.zeros((1, dimension))
+        return_dict['cca_dirns1'] = np.zeros((1, dimension))
+        return_dict['cca_dirns2'] = np.zeros((1, dimension))
 
     return return_dict
 
 
 def get_cca_similarity(acts1, acts2, threshold=0.98, compute_dirns=True,
                        verbose=True):
-    """The main function for computing cca similarities.
+    '''The main function for computing cca similarities.
 
     This function computes the cca similarity between two sets of activations,
     returning a dict with the cca coefficients, a few statistics of the cca
@@ -256,13 +256,13 @@ def get_cca_similarity(acts1, acts2, threshold=0.98, compute_dirns=True,
                            if compute_dirns=False), and summary statistics. If
                            compute_dirns=True, the cca directions are also
                            computed.
-    """
+    '''
 
     # assert dimensionality equal
-    assert acts1.shape[1] == acts2.shape[1], "dimensions don't match"
+    assert acts1.shape[1] == acts2.shape[1], 'dimensions don't match'
     # check that acts1, acts2 are transposition
-    assert acts1.shape[0] < acts1.shape[1], ("input must be number of neurons"
-                                             "by datapoints")
+    assert acts1.shape[0] < acts1.shape[1], ('input must be number of neurons'
+                                             'by datapoints')
     return_dict = {}
 
     # compute covariance with numpy function for extra stability
@@ -299,25 +299,25 @@ def get_cca_similarity(acts1, acts2, threshold=0.98, compute_dirns=True,
     idx1 = sum_threshold(sx, threshold)
     idx2 = sum_threshold(sy, threshold)
 
-    return_dict["neuron_coeffs1"] = np.dot(vx, invsqrt_xx)
-    return_dict["neuron_coeffs2"] = np.dot(vy, invsqrt_yy)
-    return_dict["cca_coef1"] = sx
-    return_dict["cca_coef2"] = sy
-    return_dict["x_idxs"] = x_idxs
-    return_dict["y_idxs"] = y_idxs
+    return_dict['neuron_coeffs1'] = np.dot(vx, invsqrt_xx)
+    return_dict['neuron_coeffs2'] = np.dot(vy, invsqrt_yy)
+    return_dict['cca_coef1'] = sx
+    return_dict['cca_coef2'] = sy
+    return_dict['x_idxs'] = x_idxs
+    return_dict['y_idxs'] = y_idxs
     # summary statistics
-    return_dict["mean"] = (np.mean(sx[:idx1]), np.mean(sy[:idx2]))
-    return_dict["sum"] = (np.sum(sx), np.sum(sy))
+    return_dict['mean'] = (np.mean(sx[:idx1]), np.mean(sy[:idx2]))
+    return_dict['sum'] = (np.sum(sx), np.sum(sy))
 
     if compute_dirns:
-        return_dict["cca_dirns1"] = cca_dirns1
-        return_dict["cca_dirns2"] = cca_dirns2
+        return_dict['cca_dirns1'] = cca_dirns1
+        return_dict['cca_dirns2'] = cca_dirns2
 
     return return_dict
 
 
 def robust_cca_similarity(acts1, acts2, threshold=0.98, compute_dirns=True):
-    """Calls get_cca_similarity multiple times while adding noise.
+    '''Calls get_cca_similarity multiple times while adding noise.
 
     This function is very similar to get_cca_similarity, and can be used if
     get_cca_similarity doesn't converge for some pair of inputs. This function
@@ -350,7 +350,7 @@ def robust_cca_similarity(acts1, acts2, threshold=0.98, compute_dirns=True):
                            if compute_dirns=False), and summary statistics. If
                            compute_dirns=True, the cca directions are also
                            computed.
-    """
+    '''
 
     for trial in range(num_cca_trials):
         try:
--- dft_ccas.py
+++ dft_ccas.py
@@ -1,19 +1,19 @@
 # Copyright 2016 Google Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 
-"""
+'''
 Code for use with large conv layers using DFT (discrete fourier transform).
 
 The functions in this script provide a scalable method for computing the cca
@@ -39,7 +39,7 @@ output the correlation of the vector with the dc component of the DFT.
 This can be seen as a lower bound on the correlation of the vector with the
 channels.
 
-"""
+'''
 
 
 import numpy as np
@@ -48,7 +48,7 @@ import svcca.cca_core as cca_core
 
 
 def fft_resize(images, resize=False, new_size=None):
-    """Function for applying DFT and resizing.
+    '''Function for applying DFT and resizing.
 
   This function takes in an array of images, applies the 2-d fourier transform
   and resizes them according to new_size, keeping the frequencies that overlap
@@ -63,18 +63,18 @@ def fft_resize(images, resize=False, new_size=None):
   Returns:
       im_fft_downsampled: a numpy array with shape
       [batch_size, (new) height, (new) width, num_channels]
-      """
-  assert len(images.shape) == 4, ("expecting images to be"
-                                  "[batch_size, height, width, num_channels]")
+      '''
+  assert len(images.shape) == 4, ('expecting images to be'
+                                  '[batch_size, height, width, num_channels]')
 
-  im_complex = images.astype("complex64")
+  im_complex = images.astype('complex64')
   im_fft = np.fft.fft2(im_complex, axes=(1, 2))
 
   # resizing images
   if resize:
     # get fourier frequencies to threshold
-    assert (im_fft.shape[1] == im_fft.shape[2]), ("Need images to have same"
-                                                  "height and width")
+    assert (im_fft.shape[1] == im_fft.shape[2]), ('Need images to have same'
+                                                  'height and width')
     # downsample by threshold
     width = im_fft.shape[2]
     new_width = new_size[0]
@@ -91,7 +91,7 @@ def fft_resize(images, resize=False, new_size=None):
 
 def fourier_ccas(conv_acts1, conv_acts2, return_coefs=False,
                  compute_dirns=False, verbose=False):
-  """Computes cca similarity between two conv layers with DFT.
+  '''Computes cca similarity between two conv layers with DFT.
 
   This function takes in two sets of convolutional activations, conv_acts1,
   conv_acts2 After resizing the spatial dimensions to be the same, applies fft
@@ -117,7 +117,7 @@ def fourier_ccas(conv_acts1, conv_acts2, return_coefs=False,
                          on the fly if compute_dirns=False), and summary
                          statistics. If compute_dirns=True, the cca directions
                          are also computed.
-  """
+  '''
 
   height1, width1 = conv_acts1.shape[1], conv_acts1.shape[2]
   height2, width2 = conv_acts2.shape[1], conv_acts2.shape[2]
@@ -146,20 +146,20 @@ def fourier_ccas(conv_acts1, conv_acts2, return_coefs=False,
 
       # apply inverse FFT to get coefficients and directions if specified
       if return_coefs:
-        results_dict["neuron_coeffs1"] = np.fft.ifft2(
-          results_dict["neuron_coeffs1"])
-        results_dict["neuron_coeffs2"] = np.fft.ifft2(
-          results_dict["neuron_coeffs2"])
+        results_dict['neuron_coeffs1'] = np.fft.ifft2(
+          results_dict['neuron_coeffs1'])
+        results_dict['neuron_coeffs2'] = np.fft.ifft2(
+          results_dict['neuron_coeffs2'])
       else:
-        del results_dict["neuron_coeffs1"]
-        del results_dict["neuron_coeffs2"]
+        del results_dict['neuron_coeffs1']
+        del results_dict['neuron_coeffs2']
 
       if compute_dirns:
-        results_dict["cca_dirns1"] = np.fft.ifft2(results_dict["cca_dirns1"])
-        results_dict["cca_dirns2"] = np.fft.ifft2(results_dict["cca_dirns2"])
+        results_dict['cca_dirns1'] = np.fft.ifft2(results_dict['cca_dirns1'])
+        results_dict['cca_dirns2'] = np.fft.ifft2(results_dict['cca_dirns2'])
 
       # accumulate results
-      results_dict["location"] = (i, j)
+      results_dict['location'] = (i, j)
       all_results = all_results.append(results_dict, ignore_index=True)
 
   return all_results
