9642a42807e1681b91366463113642c51b854c6d
Rasmus Diederichsen <rasmus@peltarion.com>
2018-07-20 18:30:11 +0200

    Complete translation to torch (many things yet to fix)

--- dft_ccas_torch.py
+++ dft_ccas_torch.py
@@ -1,6 +1,9 @@
 import torch
 import utils
 
+import pandas as pd
+from cca_core_torch import get_cca_similarity_torch
+
 
 def fft_resize_torch(images, resize=False, new_size=None):
     assert len(images.shape) == 4, ('expecting images to be'
@@ -23,3 +26,54 @@ def fft_resize_torch(images, resize=False, new_size=None):
         im_fft_downsampled = im_fft
 
     return im_fft_downsampled
+
+
+def fourier_ccas_torch(conv_acts1, conv_acts2, return_coefs=False,
+                       compute_dirns=False, verbose=False):
+    height1, width1 = conv_acts1.shape[1], conv_acts1.shape[2]
+    height2, width2 = conv_acts2.shape[1], conv_acts2.shape[2]
+    if height1 != height2 or width1 != width2:
+        height   = min(height1, height2)
+        width    = min(width1, width2)
+        new_size = [height, width]
+        resize   = True
+    else:
+        height   = height1
+        width    = width1
+        new_size = None
+        resize   = False
+
+    # resize and preprocess with fft
+    fft_acts1 = fft_resize_torch(conv_acts1, resize=resize, new_size=new_size)
+    fft_acts2 = fft_resize_torch(conv_acts2, resize=resize, new_size=new_size)
+
+    # loop over spatial dimensions and get cca coefficients
+    all_results = pd.DataFrame()
+    for i in range(height):
+        for j in range(width):
+            results_dict = get_cca_similarity_torch(
+                fft_acts1[:, i, j, :].transpose(0, 1),
+                fft_acts2[:, i, j, :].transpose(0, 1),
+                compute_dirns=compute_dirns,
+                verbose=verbose
+            )
+
+            # apply inverse FFT to get coefficients and directions if specified
+            if return_coefs:
+                results_dict['neuron_coeffs1'] = torch.irfft(results_dict['neuron_coeffs1'],
+                                                             signal_ndim=2)
+                results_dict['neuron_coeffs2'] = torch.irfft(results_dict['neuron_coeffs2'],
+                                                             signal_ndim=2)
+            else:
+                del results_dict['neuron_coeffs1']
+                del results_dict['neuron_coeffs2']
+
+            if compute_dirns:
+                results_dict['cca_dirns1'] = torch.irfft(results_dict['cca_dirns1'], signal_ndim=2)
+                results_dict['cca_dirns2'] = torch.irfft(results_dict['cca_dirns2'], signal_ndim=2)
+
+            # accumulate results
+            results_dict['location'] = (i, j)
+            all_results = all_results.append(results_dict, ignore_index=True)
+
+    return all_results
